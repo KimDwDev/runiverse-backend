f5c87d69df1206cda111bfb4a1317ce3e4f1bed7
KimDwDev <dongwan123456789@gmail.com>
2026-07-24 11:36:39 +0900

    📍 Feat: 회원가입 로직 구현

--- running-service/src/main/java/com/runiverse/running_service/application/user/command/signup/SignUpHandler.java
+++ running-service/src/main/java/com/runiverse/running_service/application/user/command/signup/SignUpHandler.java
@@ -5,6 +5,8 @@ import com.runiverse.running_service.application.user.port.out.CheckEmailDuplica
 import com.runiverse.running_service.application.user.port.out.GenerateUserIdPort;
 import com.runiverse.running_service.application.user.port.out.PasswordHashPort;
 import com.runiverse.running_service.application.user.port.out.SaveUserPort;
+import com.runiverse.running_service.domain.user.aggregate.User;
+import com.sun.jdi.request.DuplicateRequestException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,18 +25,24 @@ public class SignUpHandler implements SignUpUsecase {
 
     @Override
     public SignUpResult handle(SignUpCommand command) {
-        System.out.println("작동중");
-
         // 1. 이메일 중복 확인
+        boolean emailExists = checkEmailDuplicatePort.existsByEmail(command.email());
+        if (emailExists) throw new DuplicateRequestException("이메일이 존재합니다.");
+
         // 2. 비밀번호 해시화
+        String hashedPassword = passwordHashPort.hash(command.password());
+
         // 3. UUIDv7 생성
+        UUID userId = generateUserIdPort.generate();
+
         // 4. 도메인 User 생성
+        User user = new User(userId, command.email(), hashedPassword);
+
         // 5. DB 저장
+        User savedUser = saveUserPort.save(user);
+
         // 6. 결과 반환
-
-        UUID userId = UUID.randomUUID();
-
-        return new SignUpResult(userId);
+        return new SignUpResult(savedUser.getUserId().value());
     }
 
 }
