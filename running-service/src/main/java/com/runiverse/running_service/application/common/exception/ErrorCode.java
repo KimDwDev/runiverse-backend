d31d7097ca904b60e0972c72cd8e0a862e36064e
KimDwDev <dongwan123456789@gmail.com>
2026-07-25 20:12:14 +0900

    📍 Feat: loginHandler 구현
    - LoginHandler usecase 작성 완료
    - 에러 코드 작성

--- running-service/src/main/java/com/runiverse/running_service/application/common/exception/ErrorCode.java
+++ running-service/src/main/java/com/runiverse/running_service/application/common/exception/ErrorCode.java
@@ -6,7 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    EMAIL_ALREADY_EXISTS("U101", "이미 사용 중인 이메일입니다.");
+    EMAIL_ALREADY_EXISTS("U101", "이미 사용 중인 이메일입니다."),
+    INVALID_EMAIL_CREDENTIALS("U102", "이메일이 존재하지 않습니다."),
+    INVALID_PASSWORD_CREDENTIALS("U103", "비밀번호가 올바르지 않습니다.");
     private final String code;
     private final String message;
 }
--- running-service/src/main/java/com/runiverse/running_service/application/user/command/login/LoginResult.java
+++ running-service/src/main/java/com/runiverse/running_service/application/user/command/login/LoginResult.java
@@ -3,6 +3,8 @@ package com.runiverse.running_service.application.user.command.login;
 import java.util.UUID;
 
 public record LoginResult(
-        UUID userId
+        UUID userId,
+        String accessToken,
+        String refreshToken
 ) {
 }
