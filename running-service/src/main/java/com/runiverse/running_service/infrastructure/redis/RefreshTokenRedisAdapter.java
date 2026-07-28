4d8c7c890ba90bf820d90408d6c3c6838fb7c5b3
KimDwDev <dongwan123456789@gmail.com>
2026-07-28 17:38:50 +0900

    📍 Feat: refresh token과 관련된 adapter 구현
    - 기존의 load, delete 부분 adapter 구현

--- running-service/src/main/java/com/runiverse/running_service/infrastructure/redis/RefreshTokenRedisAdapter.java
+++ running-service/src/main/java/com/runiverse/running_service/infrastructure/redis/RefreshTokenRedisAdapter.java
@@ -1,5 +1,7 @@
 package com.runiverse.running_service.infrastructure.redis;
 
+import com.runiverse.running_service.application.auth.port.out.DeleteRefreshTokenPort;
+import com.runiverse.running_service.application.auth.port.out.LoadRefreshTokenPort;
 import com.runiverse.running_service.application.auth.port.out.SaveRefreshTokenHashPort;
 import com.runiverse.running_service.domain.user.vo.UserId;
 import com.runiverse.running_service.infrastructure.security.jwt.JwtProperties;
@@ -11,10 +13,12 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.HexFormat;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
-public class RefreshTokenRedisAdapter implements SaveRefreshTokenHashPort {
+public class RefreshTokenRedisAdapter implements SaveRefreshTokenHashPort, LoadRefreshTokenPort, DeleteRefreshTokenPort {
+
     private final StringRedisTemplate redisTemplate;
     private final JwtProperties jwtProperties;
     private static final String REFRESH_TOKEN = "refresh_token";
@@ -22,9 +26,23 @@ public class RefreshTokenRedisAdapter implements SaveRefreshTokenHashPort {
     @Override
     public void save(UserId userId, String hashedRefreshToken) {
         redisTemplate.opsForValue().set(
-                RedisKey.USER.of(userId.value().toString(), REFRESH_TOKEN),
+                key(userId),
                 hashedRefreshToken,
                 jwtProperties.refreshToken().ttl()
         );
+    }
+
+    @Override
+    public Optional<String> load(UserId userId) {
+        return Optional.ofNullable(redisTemplate.opsForValue().get(key(userId)));
+    }
+
+    @Override
+    public void delete(UserId userId) {
+        redisTemplate.delete(key(userId));
+    }
+
+    private String key(UserId userId) {
+        return RedisKey.USER.of(userId.value().toString(), REFRESH_TOKEN);
     }
 }
