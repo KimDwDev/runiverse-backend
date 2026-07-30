bd4683ced3fc05c33062bd4f89d40e837e43e8ca
KimDwDev <dongwan123456789@gmail.com>
2026-07-30 11:13:55 +0900

    📍 Feat: oauth user resolver 부분 구현 및 LoadUserByProviderport 부분 구현

--- running-service/src/main/java/com/runiverse/running_service/application/auth/command/oauthlogin/OauthUserResolver.java
+++ running-service/src/main/java/com/runiverse/running_service/application/auth/command/oauthlogin/OauthUserResolver.java
@@ -12,6 +12,22 @@ public class OauthUserResolver {
 
     @Transactional
     public User findOrRegister(OauthProfile oauthProfile) {
+        // 1. (provider, providerId)로 기존 유저 조회
+
+        // 2. 있으면 그대로 반환
+
+        // 3. 없으면 가입
+        return null;
+    }
+
+    public User register(OauthProfile oauthProfile) {
+        // 1. 기존 로컬 계정과 이메일이 겹치면 자동 연동하지 않는다
+
+        // 2.UUIDv7 발급
+
+        // 3. 유저 생성 + 소셜 연결
+
+        // 4. 저장
         return null;
     }
 
