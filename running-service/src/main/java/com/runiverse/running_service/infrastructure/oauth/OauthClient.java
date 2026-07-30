cd3ee132b5e4dcd17283b875cb2a763ef44dc021
KimDwDev <dongwan123456789@gmail.com>
2026-07-30 11:40:00 +0900

    📍 Feat: oauth client 부분 구현

---
+++ running-service/src/main/java/com/runiverse/running_service/infrastructure/oauth/OauthClient.java
@@ -0,0 +1,9 @@
+package com.runiverse.running_service.infrastructure.oauth;
+
+import com.runiverse.running_service.application.auth.port.out.OauthProfile;
+import com.runiverse.running_service.domain.user.vo.Provider;
+
+public interface OauthClient {
+    Provider provider();
+    OauthProfile exchange(String authorizationCode, String codeVerifier);
+}
