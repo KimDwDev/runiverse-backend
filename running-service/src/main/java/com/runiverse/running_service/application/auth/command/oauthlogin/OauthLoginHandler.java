e07fa592fa4aa1c5b1b2a48065c1117ca10d2e45
KimDwDev <dongwan123456789@gmail.com>
2026-07-30 17:11:45 +0900

    📍 Feat: oauthLoginHandler 부분 작성

--- running-service/src/main/java/com/runiverse/running_service/application/auth/command/oauthlogin/OauthLoginHandler.java
+++ running-service/src/main/java/com/runiverse/running_service/application/auth/command/oauthlogin/OauthLoginHandler.java
@@ -1,6 +1,8 @@
 package com.runiverse.running_service.application.auth.command.oauthlogin;
 
 import com.runiverse.running_service.application.auth.port.in.OauthLoginUsecase;
+import com.runiverse.running_service.application.auth.port.out.*;
+import com.runiverse.running_service.domain.user.aggregate.User;
 import com.runiverse.running_service.domain.user.vo.Provider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,21 +12,36 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class OauthLoginHandler implements OauthLoginUsecase {
 
+    private final ExchangeOauthCodePort exchangeOauthCodePort;
+    private final OauthUserResolver oauthUserResolver;
+    private final GenerateTokenPort generateTokenPort;
+    private final RefreshTokenHashPort refreshTokenHashPort;
+    private final SaveRefreshTokenHashPort saveRefreshTokenHashPort;
+
     @Override
     public OauthLoginResult handle(OauthLoginCommand command) {
         // 1. provider 검증
         Provider provider = Provider.from(command.provider());
 
         // 2. 인가 코드 + verifier
+        OauthProfile oauthProfile = exchangeOauthCodePort.exchange(
+                provider,
+                command.authorizationCode(),
+                command.codeVerifier()
+        );
 
         // 3. 조회 or 가입 (트랜잭션)
+        User user = oauthUserResolver.findOrRegister(oauthProfile);
 
         // 4. jwt 토큰 생성
+        String accessToken = generateTokenPort.generateAccessToken(user.getUserId());
+        String refreshToken = generateTokenPort.generateRefreshToken(user.getUserId());
 
         // 5. refresh token 해시 후 저장
+        saveRefreshTokenHashPort.save(user.getUserId(), refreshTokenHashPort.hash(refreshToken));
 
         // 6. 반환
-        return null;
+        return new OauthLoginResult(user.getUserId().value(), accessToken, refreshToken);
     }
 
 }
