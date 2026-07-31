f9b7b45d42b0630b9a05eb2a0f52e5454fe3dfb6
KimDwDev <dongwan123456789@gmail.com>
2026-07-31 14:36:37 +0900

    📍 Feat: height 부분 vo 구현

---
+++ running-service/src/main/java/com/runiverse/running_service/domain/user/exception/HeightOutOfRangeException.java
@@ -0,0 +1,8 @@
+package com.runiverse.running_service.domain.user.exception;
+
+import com.runiverse.running_service.domain.common.exception.BusinessException;
+import com.runiverse.running_service.domain.common.exception.ErrorCode;
+
+public class HeightOutOfRangeException extends BusinessException {
+    public HeightOutOfRangeException() {super(ErrorCode.HEIGHT_OUT_OF_RANGE);}
+}
