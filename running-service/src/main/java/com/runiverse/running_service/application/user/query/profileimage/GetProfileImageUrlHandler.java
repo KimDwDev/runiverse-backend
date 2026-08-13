c25482954d14414dc37651cc46990f8b4abf4336
KimDwDev <dongwan123456789@gmail.com>
2026-08-13 19:33:12 +0900

    📍 Feat: view url관련 port adapter 구현

---
+++ running-service/src/main/java/com/runiverse/running_service/application/user/query/profileimage/GetProfileImageUrlHandler.java
@@ -0,0 +1,26 @@
+package com.runiverse.running_service.application.user.query.profileimage;
+
+import com.runiverse.running_service.application.user.port.in.GetProfileImageUsecase;
+import com.runiverse.running_service.application.user.port.out.GenerateViewUrlPort;
+import com.runiverse.running_service.application.user.port.out.LoadUserByIdPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class GetProfileImageUrlHandler implements GetProfileImageUsecase {
+
+    private final LoadUserByIdPort loadUserByIdPort;
+    private final GenerateViewUrlPort generateViewUrlPort;
+
+    @Override
+    public GetProfileImageUrlResult handle(GetProfileImageUrlQuery query) {
+
+        // 1. 대상 사용자 조회
+
+        // 2. 이미지가 없으면 URL도 없다
+        return null;
+    }
+}
