bf09497b0fac6d695516d4a649a790e047d57b5b
KimDwDev <dongwan123456789@gmail.com>
2026-08-13 19:38:30 +0900

    📍 Feat: profile_image_url 확인 handler 구현

--- running-service/src/main/java/com/runiverse/running_service/application/user/query/profileimage/GetProfileImageUrlHandler.java
+++ running-service/src/main/java/com/runiverse/running_service/application/user/query/profileimage/GetProfileImageUrlHandler.java
@@ -1,8 +1,12 @@
 package com.runiverse.running_service.application.user.query.profileimage;
 
+import com.runiverse.running_service.application.user.exception.ProfileNotFoundException;
 import com.runiverse.running_service.application.user.port.in.GetProfileImageUsecase;
 import com.runiverse.running_service.application.user.port.out.GenerateViewUrlPort;
 import com.runiverse.running_service.application.user.port.out.LoadUserByIdPort;
+import com.runiverse.running_service.domain.user.aggregate.User;
+import com.runiverse.running_service.domain.user.vo.ProfileImageKey;
+import com.runiverse.running_service.domain.user.vo.UserId;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,8 +23,14 @@ public class GetProfileImageUrlHandler implements GetProfileImageUsecase {
     public GetProfileImageUrlResult handle(GetProfileImageUrlQuery query) {
 
         // 1. 대상 사용자 조회
+        User user = loadUserByIdPort.loadById(new UserId(query.userId()))
+                .orElseThrow(ProfileNotFoundException::new);
 
         // 2. 이미지가 없으면 URL도 없다
-        return null;
+        String profileImageUrl = user.getProfileImageKey()
+                .map(ProfileImageKey::value)
+                .map(generateViewUrlPort::generate)
+                .orElse(null);
+        return new GetProfileImageUrlResult(query.userId(), profileImageUrl);
     }
 }
