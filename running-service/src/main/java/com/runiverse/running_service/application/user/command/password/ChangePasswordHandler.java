7b6e773310583fd46fb08ee1d34c28813ad0d458
KimDwDev <dongwan123456789@gmail.com>
2026-08-17 11:25:34 +0900

    📍 Feat: 비밀번호 변경과 관련된 handler 구현

--- running-service/src/main/java/com/runiverse/running_service/application/user/command/password/ChangePasswordHandler.java
+++ running-service/src/main/java/com/runiverse/running_service/application/user/command/password/ChangePasswordHandler.java
@@ -1,9 +1,14 @@
 package com.runiverse.running_service.application.user.command.password;
 
 import com.runiverse.running_service.application.common.port.out.PasswordHashPort;
+import com.runiverse.running_service.application.user.exception.InvalidCurrentPasswordException;
+import com.runiverse.running_service.application.user.exception.PasswordNotSetException;
+import com.runiverse.running_service.application.user.exception.UserNotFoundException;
 import com.runiverse.running_service.application.user.port.in.ChangePasswordUsecase;
 import com.runiverse.running_service.application.user.port.out.LoadUserByIdPort;
 import com.runiverse.running_service.application.user.port.out.UpdatePasswordPort;
+import com.runiverse.running_service.domain.user.aggregate.User;
+import com.runiverse.running_service.domain.user.vo.UserId;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,14 +24,25 @@ public class ChangePasswordHandler implements ChangePasswordUsecase {
 
     @Override
     public void handle(ChangePasswordCommand command) {
+        UserId userId = new UserId(command.userId());
         // 1. 유저 조회
+        User user = loadUserByIdPort.loadById(userId)
+                .orElseThrow(UserNotFoundException::new);
 
         // 2. 소설 전용 계정은 바꾸지 않는다
+        if (user.isPasswordNotSet()) {
+            throw new PasswordNotSetException();
+        }
 
         // 3. 현재 비밀번호 확인
+        if (!passwordHashPort.matches(command.currentPassword(), user.getPasswordHash().value())) {
+            throw new InvalidCurrentPasswordException();
+        }
 
         // 4. 도메인에서 빈 해시 검증
+        user.changePassword(passwordHashPort.hash(command.newPassword()));
 
         // 5. 갱신
+        updatePasswordPort.updatePassword(userId, user.getPasswordHash());
     }
 }
