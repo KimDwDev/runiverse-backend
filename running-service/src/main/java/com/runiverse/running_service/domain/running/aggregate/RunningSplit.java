d34673ceb2d69b25fe5b163cb2be0227b19dd304
KimDwDev <dongwan123456789@gmail.com>
2026-08-19 15:47:27 +0900

    📍 Feat: running split 관련 어그리게이트 구현

---
+++ running-service/src/main/java/com/runiverse/running_service/domain/running/aggregate/RunningSplit.java
@@ -0,0 +1,58 @@
+package com.runiverse.running_service.domain.running.aggregate;
+
+import com.runiverse.running_service.domain.running.vo.Cadence;
+import com.runiverse.running_service.domain.running.vo.Calories;
+import com.runiverse.running_service.domain.running.vo.Distance;
+import com.runiverse.running_service.domain.running.vo.ElapsedTime;
+import com.runiverse.running_service.domain.running.vo.ElevationChange;
+import com.runiverse.running_service.domain.running.vo.Pace;
+import com.runiverse.running_service.domain.running.vo.RouteRange;
+import com.runiverse.running_service.domain.running.vo.RunningPeriod;
+import com.runiverse.running_service.domain.running.vo.SplitNumber;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Getter
+public class RunningSplit {
+
+    private final SplitNumber splitNumber;
+    private final Pace avgPace;
+    private final Distance distance;
+    private final ElapsedTime duration;
+    private final RouteRange routeRange;
+    private final RunningPeriod period;
+    private final Calories calories;
+    // 선택 항목
+    private final Cadence avgCadence;
+    private final ElevationChange elevationChange;
+
+    @Builder(access = AccessLevel.PACKAGE)
+    private RunningSplit(int splitNumber, int avgPace, int distance, int duration,
+                         int routeStartIndex, int routeEndIndex,
+                         LocalDateTime startAt, LocalDateTime endAt,
+                         int calories,
+                         Integer avgCadence, Integer elevationChange) {
+        this.splitNumber = new SplitNumber(splitNumber);
+        this.avgPace = new Pace(avgPace);
+        this.distance = new Distance(distance);
+        this.duration = new ElapsedTime(duration);
+        this.routeRange = new RouteRange(routeStartIndex, routeEndIndex);
+        this.period = new RunningPeriod(startAt, endAt);
+        this.avgCadence = avgCadence == null ? null : new Cadence(avgCadence);
+        this.elevationChange = elevationChange == null ? null : new ElevationChange(elevationChange);
+        this.calories = new Calories(calories);
+    }
+
+    // cadence는 있을 수도 없을 수도
+    public Optional<Cadence> getAvgCadence() {
+        return Optional.ofNullable(avgCadence);
+    }
+
+    public Optional<ElevationChange> getElevationChange() {
+        return Optional.ofNullable(elevationChange);
+    }
+}
