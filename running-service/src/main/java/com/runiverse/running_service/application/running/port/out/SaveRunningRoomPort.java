8034e998e235c11bdfba694135c71ea7a365c78b
KimDwDev <dongwan123456789@gmail.com>
2026-08-21 10:46:43 +0900

    📍 Feat: running solo를 위한 port 구현

---
+++ running-service/src/main/java/com/runiverse/running_service/application/running/port/out/SaveRunningRoomPort.java
@@ -0,0 +1,8 @@
+package com.runiverse.running_service.application.running.port.out;
+
+import com.runiverse.running_service.domain.running.aggregate.RunningRoom;
+
+public interface SaveRunningRoomPort {
+
+    RunningRoom save(RunningRoom room);
+}
