9ab283b3b7d84e4f60a175ed7ad07eee669fe883
KimDwDev <dongwan123456789@gmail.com>
2026-08-21 14:34:44 +0900

    📍 Feat: solo 러닝과 관련된 usecase, command, result 구현

---
+++ running-service/src/main/java/com/runiverse/running_service/application/running/command/solo/StartSoloRunningResult.java
@@ -0,0 +1,7 @@
+package com.runiverse.running_service.application.running.command.solo;
+
+import java.time.LocalDateTime;
+
+public record StartSoloRunningResult(Long runningRoomId, LocalDateTime startedAt) {
+
+}
