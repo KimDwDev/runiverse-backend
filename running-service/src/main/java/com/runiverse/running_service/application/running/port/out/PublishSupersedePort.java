613e20476840fc300ef18d3b98addbcc6c8afdb8
KimDwDev <dongwan123456789@gmail.com>
2026-08-25 14:24:59 +0900

    📍 Feat: running에 publish 부분 구현

---
+++ running-service/src/main/java/com/runiverse/running_service/application/running/port/out/PublishSupersedePort.java
@@ -0,0 +1,9 @@
+package com.runiverse.running_service.application.running.port.out;
+
+import java.util.UUID;
+
+public interface PublishSupersedePort {
+
+    // 이 유저의 진짜 연결은 winnerSessionId 하나라고 다른 인스턴스들에게 알린다.
+    void publish(UUID userId, String winnerSessionId);
+}
