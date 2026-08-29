4143be0571ec29e4ea8299c8822a9449e751799e
KimDwDev <dongwan123456789@gmail.com>
2026-08-30 01:46:27 +0900

    📍 Feat: running split 전달하는 부분 구현

--- running-service/src/main/java/com/runiverse/running_service/presentation/running/controller/RunningRoomController.java
+++ running-service/src/main/java/com/runiverse/running_service/presentation/running/controller/RunningRoomController.java
@@ -3,10 +3,14 @@ package com.runiverse.running_service.presentation.running.controller;
 import com.runiverse.running_service.application.running.command.solo.OpenSoloRoomCommand;
 import com.runiverse.running_service.application.running.command.solo.OpenSoloRoomResult;
 import com.runiverse.running_service.application.running.port.in.GetRunningResultsUsecase;
+import com.runiverse.running_service.application.running.port.in.GetRunningSplitResultsUsecase;
 import com.runiverse.running_service.application.running.port.in.OpenSoloRoomUsecase;
 import com.runiverse.running_service.application.running.query.result.GetRunningResultsQuery;
 import com.runiverse.running_service.application.running.query.result.GetRunningResultsResult;
+import com.runiverse.running_service.application.running.query.split.GetRunningSplitResultsQuery;
+import com.runiverse.running_service.application.running.query.split.GetRunningSplitResultsResult;
 import com.runiverse.running_service.presentation.running.response.RunningResultsResponse;
+import com.runiverse.running_service.presentation.running.response.RunningSplitResultsResponse;
 import com.runiverse.running_service.presentation.running.response.SoloRunningStartResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -28,6 +32,7 @@ public class RunningRoomController {
 
     private final OpenSoloRoomUsecase startSoloRunningUsecase;
     private final GetRunningResultsUsecase getRunningResultsUsecase;
+    private final GetRunningSplitResultsUsecase getRunningSplitResultsUsecase;
 
     @PostMapping("/solo")
     public ResponseEntity<SoloRunningStartResponse> startSolo(@AuthenticationPrincipal Jwt jwt) {
@@ -48,5 +53,16 @@ public class RunningRoomController {
         GetRunningResultsResult result = getRunningResultsUsecase.handle(
                 new GetRunningResultsQuery(runningRoomId, userId));
         return ResponseEntity.ok(RunningResultsResponse.from(result));
+    }
+
+    @GetMapping("/{runningRoomId}/split-results")
+    public ResponseEntity<RunningSplitResultsResponse> getSplitResults(
+            @AuthenticationPrincipal Jwt jwt,
+            @PathVariable Long runningRoomId
+    ) {
+        UUID userId = UUID.fromString(jwt.getSubject());
+        GetRunningSplitResultsResult result = getRunningSplitResultsUsecase.handle(
+                new GetRunningSplitResultsQuery(runningRoomId, userId));
+        return ResponseEntity.ok(RunningSplitResultsResponse.from(result));
     }
 }
