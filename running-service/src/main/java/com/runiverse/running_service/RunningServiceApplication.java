49800d2872e1dd1ee4591adc6251e11c285978ad
KimDwDev <dongwan123456789@gmail.com>
2026-09-04 15:05:51 +0900

    📍 Feat: running에 스케줄링 부분 설정

--- running-service/src/main/java/com/runiverse/running_service/RunningServiceApplication.java
+++ running-service/src/main/java/com/runiverse/running_service/RunningServiceApplication.java
@@ -3,11 +3,9 @@ package com.runiverse.running_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EnableScheduling
 public class RunningServiceApplication {
 
     public static void main(String[] args) {
