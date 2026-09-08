4015af2eeb4cb79c614b3aa7a6315a845bd48556
KimDwDev <dongwan123456789@gmail.com>
2026-09-08 17:01:13 +0900

    ✅ Test: match와 관련된 통합테스트 진행

--- running-service/src/test/java/com/runiverse/running_service/unit_test/user/domain/aggregate/OauthUserTest.java
+++ running-service/src/test/java/com/runiverse/running_service/unit_test/user/domain/aggregate/OauthUserTest.java
@@ -1,4 +1,4 @@
-package com.runiverse.running_service.domain.user.aggregate;
+package com.runiverse.running_service.domain.user;
 
 import com.runiverse.running_service.domain.user.OauthUser;
 import com.runiverse.running_service.domain.user.exception.ProviderIdRequiredException;
